8823da449b6a3b0163b6708266c6acb4e5116f5f
2011 <judong1094@gmail.com>
2024-12-22 21:11:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 
 # 엔트리 이야기 글쓰기
 user = dut.signin('USERNAME', 'PASSWORD')
-print(dut.write(user, 'CONTENT'))
+print(dut.write(user, 'CONTENT')) # sticker등 더 많은 요소 사용 가능
 
 # 실시간 변수 설정 및 값 조회
 user = dut.signin('USERNAME', 'PASSWORD')
@@ -15,7 +15,10 @@ user = dut.signin('USERNAME', 'PASSWORD')
 print(f'로그인 결과\nSession: {user.session}\nCsrf_Token: {user.csrf_token}\nX_Token: {user.x_token}')
 
 # 회원가입
-user = json.loads(dut.signup())
+user = json.loads(dut.signup()) 
+# username, password, email 설정 가능
+# 입력 안했을시, 임의의 문자열과 일회용 메일로 생성되고 인증됨
+
 username = user.get('username')
 password = user.get('password')
 print(f'계정 생성 결과\n아이디: {username}\n암호: {password}')
